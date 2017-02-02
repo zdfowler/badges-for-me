436fa98e0f2a7f394fc75b6b452d7977fef7163e
Zac Fowler <zfowler@unomaha.edu>
2017-02-02 11:04:18 -0600

    Added const list of Heroes

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -10,8 +10,11 @@ import { Component } from '@angular/core';
     	<label>name: </label>
     	<input [(ngModel)]="hero.name" placeholder="name">
   	</div>
-  `
+  `,
+
 })
+
+
 export class AppComponent  { 
 	name = 'Angular'; 
 	title = 'Tour of Heroes';
@@ -19,6 +22,7 @@ export class AppComponent  {
 		id: 1,
 		name: 'Windstorm'
 	};
+	heroes = HEROES;
 }
 
 export class Hero {
@@ -27,3 +31,15 @@ export class Hero {
 }
 
 
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
