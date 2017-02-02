57359106a4e0884f5be3875bd58b57a59277644a
Zac Fowler <zfowler@unomaha.edu>
2017-02-02 11:08:53 -0600

    Added loop to show heroes

--- quickstart/app/app.component.ts
+++ quickstart/app/app.component.ts
@@ -4,12 +4,20 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template:`
 	<h1>{{title}}</h1>
+	  	<h2>My Heroes</h2>
+	<ul class="heroes">
+  		<li *ngFor="let hero of heroes">
+    		<span class="badge">{{hero.id}}</span> {{hero.name}}
+  		</li>
+	</ul>
 	<h2>{{hero.name}} details!</h2>
 	<div><label>id: </label>{{hero.id}}</div>
 	<div>
     	<label>name: </label>
     	<input [(ngModel)]="hero.name" placeholder="name">
   	</div>
+
+
   `,
 
 })
